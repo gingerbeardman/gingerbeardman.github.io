b543717eda766acc3af65424d4e009d670957a72
Matt Sephton <matt.sephton@gmail.com>
2024-08-19 19:26:10 +0100

    simplify

--- _plugins/replace_nouns.rb
+++ _plugins/replace_nouns.rb
@@ -3,8 +3,7 @@ module Jekyll
     def replace_nouns(content, nouns)
       return content if nouns.nil? || nouns.empty?
       nouns.each do |noun|
-        noun_str = noun.to_s
-        content = content.gsub(/\b#{Regexp.escape(noun_str)}\b/, "<em>#{noun_str}</em>")
+        content = content.gsub(/\b#{Regexp.escape(noun)}\b/, "<em>#{noun}</em>")
       end
       content
     end
