2b5aec10e2fd3553dea80b5832e7a4b4839895c1
Matt Sephton <matt.sephton@gmail.com>
2024-09-03 17:15:34 +0100

    better noun replacement

--- _plugins/replace_nouns.rb
+++ _plugins/replace_nouns.rb
@@ -3,8 +3,14 @@ module Jekyll
     def replace_nouns(content, nouns)
       return content if nouns.nil? || nouns.empty?
       nouns.each do |noun|
-        noun_str = noun.to_s
-        content = content.gsub(/\b#{Regexp.escape(noun_str)}\b/, "<em>#{noun_str}</em>")
+        noun_str = Regexp.escape(noun.to_s)
+        content = content.gsub(/(?<=^|\W|\/)#{noun_str}(?=$|\W|\/)/) do |match|
+          if match.start_with?('/') || match.end_with?('/')
+            match  # Don't replace if it starts or ends with a slash
+          else
+            "<em>#{match}</em>"
+          end
+        end
       end
       content
     end
