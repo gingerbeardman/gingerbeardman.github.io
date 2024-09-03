bcd435e020c172809922876ec0e74612f96934e8
Matt Sephton <matt.sephton@gmail.com>
2024-09-03 17:45:14 +0100

    even better noun replacement

--- _plugins/replace_nouns.rb
+++ _plugins/replace_nouns.rb
@@ -4,9 +4,10 @@ module Jekyll
       return content if nouns.nil? || nouns.empty?
       nouns.each do |noun|
         noun_str = Regexp.escape(noun.to_s)
-        content = content.gsub(/(?<=^|\W|\/)#{noun_str}(?=$|\W|\/)/) do |match|
-          if match.start_with?('/') || match.end_with?('/')
-            match  # Don't replace if it starts or ends with a slash
+        content = content.gsub(/(?<=^|\W|\/|\.)#{noun_str}(?=$|\W|\/|\.)/) do |match|
+          if match.start_with?('/') || match.end_with?('/') || 
+             match.start_with?('.') || match.end_with?('.')
+            match  # Don't replace if it starts or ends with a slash or dot
           else
             "<em>#{match}</em>"
           end
