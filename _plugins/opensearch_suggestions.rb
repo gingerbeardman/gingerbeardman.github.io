a7b89951d8d00c26cbc06dee71c3492959717d05
Matt Sephton <matt.sephton@gmail.com>
2024-09-15 22:56:13 +0100

    opensearch

--- _plugins/opensearch_suggestions.rb
+++ _plugins/opensearch_suggestions.rb
@@ -12,23 +12,31 @@ module Jekyll
       site = context.registers[:site]
       sitemap_path = File.join(site.source, 'sitemap.xml')
       
+      debug_info = {
+        search_term: @search_term,
+        sitemap_path: sitemap_path,
+        sitemap_exists: File.exist?(sitemap_path)
+      }
+      
       if File.exist?(sitemap_path)
         sitemap = File.read(sitemap_path)
         doc = Nokogiri::XML(sitemap)
         
         urls = doc.xpath('//xmlns:url/xmlns:loc').map(&:text)
+        debug_info[:total_urls] = urls.length
         
         matches = urls.select { |url| url.downcase.include?(@search_term.downcase) }
+        debug_info[:matches] = matches
         
         suggestions = matches.map do |url|
           title = url.split('/').last.gsub('-', ' ').capitalize
           [title, url]
         end
         
-        result = [@search_term, suggestions.map(&:first), suggestions.map(&:last)]
+        result = [@search_term, suggestions.map(&:first), suggestions.map(&:last), debug_info]
         result.to_json
       else
-        '[]'
+        { error: "Sitemap not found", debug_info: debug_info }.to_json
       end
     end
   end
