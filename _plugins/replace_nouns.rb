29262ad226560634428a2f35e752cafebbba2ee9
Matt Sephton <matt.sephton@gmail.com>
2024-11-03 14:28:01 +0000

    fix nouns

--- _plugins/replace_nouns.rb
+++ _plugins/replace_nouns.rb
@@ -2,17 +2,28 @@ module Jekyll
   module ReplaceNounsFilter
     def replace_nouns(content, nouns)
       return content if nouns.nil? || nouns.empty?
+      
+      # First, temporarily protect URLs
+      url_placeholders = {}
+      content = content.gsub(%r{https?://[^\s<>"]+}) do |url|
+        placeholder = "URL_PLACEHOLDER_#{url_placeholders.length}"
+        url_placeholders[placeholder] = url
+        placeholder
+      end
+      
+      # Replace nouns
       nouns.each do |noun|
         noun_str = Regexp.escape(noun.to_s)
-        content = content.gsub(/(?<=^|\W|\/|\.)#{noun_str}(?=$|\W|\/|\.)/) do |match|
-          if match.start_with?('/') || match.end_with?('/') || 
-             match.start_with?('.') || match.end_with?('.')
-            match  # Don't replace if it starts or ends with a slash or dot
-          else
-            "<em>#{match}</em>"
-          end
+        content = content.gsub(/(?<=^|\W)#{noun_str}(?=$|\W)/) do |match|
+          "<em>#{match}</em>"
         end
       end
+      
+      # Restore URLs
+      url_placeholders.each do |placeholder, url|
+        content = content.gsub(placeholder, url)
+      end
+      
       content
     end
   end
